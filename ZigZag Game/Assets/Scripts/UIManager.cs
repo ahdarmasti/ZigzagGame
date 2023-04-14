3df8fd8a1f182c5e86a2b7dd855dcf1ee2fdf7bd
ahdarmasti <ahdarmasti@gmail.com>
2023-04-14 10:34:08 +0200

    Start scripting the UIManager

--- ZigZag Game/Assets/Scripts/UIManager.cs
+++ ZigZag Game/Assets/Scripts/UIManager.cs
@@ -1,15 +1,52 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
+    public static UIManager instance;
+    
+    public GameObject startPanel;
+    public GameObject gameOverPanel;
+    public GameObject tapText;
+    public TextMeshProUGUI score;
+    public TextMeshProUGUI highScore1;
+    public TextMeshProUGUI highScore2;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    public void GameStart()
+    {
+        tapText.SetActive(false);
+        startPanel.GetComponent<Animator>().Play("panelUp");
+    }
+    
+    public void GameOver()
+    {
+        gameOverPanel.SetActive(true);
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(0);
+    }
+    
     // Update is called once per frame
     void Update()
     {
